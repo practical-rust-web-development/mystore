8d391710e5940e27758763931f918037ef03b1cb
werner <werner_a_e@yahoo.es>
2019-07-04 08:42:15 -0300

    replace GIN index by RUM

--- migrations/2019-06-25-112356_add_tsvector_column_to_products/up.sql
+++ migrations/2019-06-25-112356_add_tsvector_column_to_products/up.sql
@@ -4,7 +4,7 @@ ALTER TABLE products ADD COLUMN text_searchable_product_col tsvector NOT NULL;
 UPDATE products SET text_searchable_product_col = 
     to_tsvector('english', name || ' ' || coalesce(description, ''));
 
-CREATE INDEX textsearch_idx ON products USING GIN (text_searchable_product_col);
+CREATE INDEX textsearch_idx ON products USING RUM (text_searchable_product_col rum_tsvector_ops);
 
 CREATE TRIGGER tsvectorupdateproducts BEFORE INSERT OR UPDATE
 ON products FOR EACH ROW EXECUTE PROCEDURE
