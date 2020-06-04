89cb00f00a3233329de94440bb17e55192978abb
werner <werner_a_e@yahoo.es>
2020-06-04 14:45:48 -0300

    reformat sale_product

--- src/models/sale_product.rs
+++ src/models/sale_product.rs
@@ -1,13 +1,13 @@
-use crate::schema::sale_products;
+use crate::models::product::{FormProduct, Product};
 use crate::models::sale::Sale;
-use crate::models::product::{ Product, FormProduct };
+use crate::schema::sale_products;
 
 #[derive(Identifiable, Associations, Queryable, Debug, Clone, PartialEq)]
-#[table_name="sale_products"]
+#[table_name = "sale_products"]
 #[belongs_to(Sale)]
 #[belongs_to(Product)]
 #[derive(juniper::GraphQLObject)]
-#[graphql(description="Relationship between sale and products")]
+#[graphql(description = "Relationship between sale and products")]
 pub struct SaleProduct {
     pub id: i32,
     pub product_id: i32,
@@ -16,20 +16,19 @@ pub struct SaleProduct {
     pub discount: i32,
     pub tax: i32,
     pub price: i32,
-    pub total: f64
+    pub total: f64,
 }
 
-#[derive(juniper::GraphQLObject)]
-#[derive(Debug, Clone)]
+#[derive(juniper::GraphQLObject, Debug, Clone)]
 pub struct FullSaleProduct {
     pub sale_product: SaleProduct,
-    pub product: Product
+    pub product: Product,
 }
 
 #[derive(Insertable, Deserialize, Serialize, AsChangeset, Debug, Clone, PartialEq)]
-#[table_name="sale_products"]
+#[table_name = "sale_products"]
 #[derive(juniper::GraphQLInputObject)]
-#[graphql(description="Relationship between sale and products")]
+#[graphql(description = "Relationship between sale and products")]
 pub struct FormSaleProduct {
     pub id: Option<i32>,
     pub product_id: Option<i32>,
@@ -38,15 +37,16 @@ pub struct FormSaleProduct {
     pub discount: Option<i32>,
     pub tax: Option<i32>,
     pub price: Option<i32>,
-    pub total: Option<f64>
+    pub total: Option<f64>,
 }
 
-#[derive(juniper::GraphQLInputObject)]
-#[derive(Debug, Clone)]
+#[derive(juniper::GraphQLInputObject, Debug, Clone)]
 pub struct FullFormSaleProduct {
     pub sale_product: FormSaleProduct,
-    pub product: FormProduct
+    pub product: FormProduct,
 }
 
 #[derive(juniper::GraphQLInputObject)]
-pub struct FormSaleProducts{ pub data: Vec<FullFormSaleProduct> }
+pub struct FormSaleProducts {
+    pub data: Vec<FullFormSaleProduct>,
+}
